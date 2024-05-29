6fff66d7d6a4cf5c9686db9da89d7a5c374db4a5
Florian Gilcher <florian.gilcher@ferrous-systems.com>
2024-05-29 09:51:28 +0200

    Fix small typo: "form" -> "from"

--- src/idt.rs
+++ src/idt.rs
@@ -2,7 +2,7 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-// Derived form the rxv64 operating system.
+// Derived from the rxv64 operating system.
 
 use crate::println;
 use bit_field::BitField;
